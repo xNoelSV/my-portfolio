0d4c986adcae051965b2d744685b929dedb5e6bc
xNoelSV <noelsava25@gmail.com>
2025-10-08 00:37:29 +0200

    OG fix 2.0

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -14,10 +14,7 @@ export async function GET(req: NextRequest) {
       : ["Blog", "Projects"];
 
     const robotoFlex = await fetch(
-      new URL(
-        "../../../public/_static/fonts/RobotoFlex-Regular.ttf",
-        import.meta.url
-      )
+      new URL("@/public/_static/fonts/RobotoFlex.ttf", import.meta.url)
     ).then((res) => res.arrayBuffer());
 
     return new ImageResponse(
